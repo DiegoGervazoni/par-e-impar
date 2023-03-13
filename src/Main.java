a4e295563462741b66fb4d63a23bcf4d5e5d9635
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2023-03-13 12:46:38 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        int somaPar = 0;
+        int somaImpar = 0;
+        int count = 0;
+
+        do {
+            System.out.println("Digite um valor: ");
+            numero = sc.nextInt();
+
+            if((numero % 2) == 0){
+                somaPar += 1;
+            }else{
+                somaImpar += 1;
+            }
+
+            count += 1;
+
+        }while(count <= 5);
+
+        System.out.println("Numeros pares: " + somaPar);
+        System.out.println("Numeros impares: " + somaImpar);
+    }
+}
